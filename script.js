0846275a314c2a427db672aa24f062e3451fee0f
Igor <igortadic.msc@gmail.com>
2024-10-19 23:11:23 +0200

    After button click, store the inputed value, clear the grid canvas, fetch latest computer width, create a fresh grid of pixel divs
    After the initial set-up and button click, the intial state of grid wasn't returned and the div creation loop was never re-initiated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.getElementById('container');
-const columns = 20; // Number of columns
+const columns = 3; // Number of columns
 // const rows = 10; // Number of rows
 const setResBtn = document.querySelector('#setResolutionBtn');
 
@@ -15,13 +15,37 @@ width = parseFloat(width);
 setResBtn.addEventListener('click', () => {
     let gridResolution = prompt("Set the grid dimensions: ");
     console.log(gridResolution);
+    container.style.height = `${gridResolution}px`;
+    container.style.width = `${gridResolution}px`;
+    console.log(`unutar setResBtn-a ${width}`);
+    // clear everything from grid
+    container.innerHTML = '';
+
+    // recalculate grid canvas dimension
+    width = window.getComputedStyle(document.querySelector('#container')).width
+    console.log(width);
+    // convert the numeric part into float, discard the string chars
+    width = parseFloat(width);
+    
+    // fill the grid out using new values
+    for (let i = 0; i < columns*columns; i++) {
+        const element = document.createElement('div');
+        element.classList.add('columns');
+        element.style.width = `${width/columns}px`
+        element.style.height = `${width/columns}px`
+        console.log(`in the loop ${width}`);
+    
+        container.appendChild(element);
+    }
 })
 
+// initial set-up
 for (let i = 0; i < columns*columns; i++) {
     const element = document.createElement('div');
     element.classList.add('columns');
     element.style.width = `${width/columns}px`
     element.style.height = `${width/columns}px`
+    console.log(`in the loop ${width}`);
 
     container.appendChild(element);
 }
