8e7c85688704975435ee532e9f0673db32ff1d09
Igor <igortadic.msc@gmail.com>
2024-10-19 22:29:50 +0200

    Add a variable to hold the user's wished grid resolution
    An event listener will trigger a popup for user to modify the canvas-grid resolution

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const container = document.getElementById('container');
-const columns = 7; // Number of columns
-const rows = 10; // Number of rows
+const columns = 20; // Number of columns
+// const rows = 10; // Number of rows
+const setResBtn = document.querySelector('#setResolutionBtn');
+
+
 
 // return a string representing the value of property
 let width = window.getComputedStyle(document.querySelector('#container')).width
@@ -8,12 +11,17 @@ console.log(width);
 // convert the numeric part into float, discard the string chars
 width = parseFloat(width);
 
+
+setResBtn.addEventListener('click', () => {
+    let gridResolution = prompt("Set the grid dimensions: ");
+    console.log(gridResolution);
+})
+
 for (let i = 0; i < columns*columns; i++) {
     const element = document.createElement('div');
     element.classList.add('columns');
     element.style.width = `${width/columns}px`
     element.style.height = `${width/columns}px`
 
-    
     container.appendChild(element);
 }
