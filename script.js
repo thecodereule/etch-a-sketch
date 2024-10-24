69d72ec26e11d0049ed2760eb5588e37c57832f8
Igor <igortadic.msc@gmail.com>
2024-10-25 00:01:38 +0200

    issue #2: Code responsible for creating child elements of container is being repeated twice
    encapsulated the code into createGrid function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.getElementById('container');
+const container = document.querySelector('#container');
 const columns = 3; // Number of columns
 // const rows = 10; // Number of rows
 const setResBtn = document.querySelector('#setResolutionBtn');
@@ -10,6 +10,17 @@ console.log(width);
 // convert the numeric part into float, discard the string chars
 width = parseFloat(width);
 
+function createGrid() {
+    for (let i = 0; i < columns*columns; i++) {
+        const element = document.createElement('div');
+        element.classList.add('columns');
+        element.style.width = `${width/columns}px`
+        element.style.height = `${width/columns}px`
+        console.log(`in the loop ${width}`);
+    
+        container.appendChild(element);
+    }
+}
 
 setResBtn.addEventListener('click', () => {
     let gridResolution = prompt("Set the grid dimensions: ");
@@ -28,27 +39,9 @@ setResBtn.addEventListener('click', () => {
 
     
     // fill the grid out using new values
-    for (let i = 0; i < columns*columns; i++) {
-        const element = document.createElement('div');
-        element.classList.add('columns');
-        element.style.width = `${width/columns}px`
-        element.style.height = `${width/columns}px`
-        console.log(`in the loop ${width}`);
-    
-        container.appendChild(element);
-    }
+    createGrid();
 })
 
 
 // initial set-up
-for (let i = 0; i < columns*columns; i++) {
-    const element = document.createElement('div');
-    element.classList.add('columns');
-    element.style.width = `${width/columns}px`
-    element.style.height = `${width/columns}px`
-    console.log(`in the loop ${width}`);
-
-    container.appendChild(element);
-}
-
-const pixel = document.querySelector('div#container > div');
+createGrid();
